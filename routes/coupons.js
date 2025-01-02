358383122b7233c75e0b89f8a2098603f122e66b
dev-neeraj96 <neerajbatchu96@gmail.com>
2025-01-03 00:19:18 +0530

    added add coupon functionality

--- routes/coupons.js
+++ routes/coupons.js
@@ -1,12 +1,11 @@
 const express = require("express");
-const authenticateToken = require("../middleware/authentiactToken"); // Import the middleware
 const { getAllCoupons } = require("../controller/couponsController");
 
 //router object
 const router = express.Router();
 
 //get all Coupons
-router.get("/allCoupons", authenticateToken, getAllCoupons);
+router.get("/allCoupons", getAllCoupons);
 
 
 module.exports = router
